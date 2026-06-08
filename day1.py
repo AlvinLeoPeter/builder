5ef3efa117c5f67d3f9a772b1aa36877258d8aca
Alvin Leo Peter <alvinleopeter39@Outlook.com>
2026-06-08 21:01:10 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+print(f"Let's build something great, {name}.")
+name = input("What's your name? ")
+age = input("How old are you? ")
+goal = input("What do you want to build? ")
+
+print(f"\nAlright {name}, you're {age} years old.")
+print(f"And you want to build: {goal}")
+print("Let's get to work.")
