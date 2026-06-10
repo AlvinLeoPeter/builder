91eda39afb0235db1581f9a75b05eadaae373372
Alvin Leo Peter <alvinleopeter39@Outlook.com>
2026-06-10 20:18:53 +0530

    day4 - added loop to code explainer

--- day4.py
+++ day4.py
@@ -5,15 +5,23 @@ import os
 load_dotenv()
 client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
-user_input = input("Paste your code here: ")
+while True:
 
-message = client.messages.create(
-    model="claude-sonnet-4-5",
-    max_tokens=1024,
-    system="You are a coding assistant that gives simple and understandable answers to each line of the code given to you.",
-    messages=[
+    user_input = input("Paste your code here: type 'exit' to quit:")
+
+    if user_input == "exit":
+        break
+
+    message = client.messages.create(
+        model="claude-sonnet-4-5",
+        max_tokens=4096,
+        system="You are a coding assistant that gives simple and understandable answers to each line of the code given to you.",
+        messages=[
         {"role": "user", "content": user_input}
-    ]
-)
+        ]
 
-print(message.content[0].text)
+    )
+    print(message.content[0].text)
+
+    
+
