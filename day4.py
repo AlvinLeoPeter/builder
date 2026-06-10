1404c717015ae7ac0c37112e43eb95353302544c
Alvin Leo Peter <alvinleopeter39@Outlook.com>
2026-06-10 20:03:30 +0530

    day4 - AI code explainer

---
+++ day4.py
@@ -0,0 +1,19 @@
+import anthropic
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+
+user_input = input("Paste your code here: ")
+
+message = client.messages.create(
+    model="claude-sonnet-4-5",
+    max_tokens=1024,
+    system="You are a coding assistant that gives simple and understandable answers to each line of the code given to you.",
+    messages=[
+        {"role": "user", "content": user_input}
+    ]
+)
+
+print(message.content[0].text)
